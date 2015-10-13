c6ff6e04e55f3636ecfb342eadbcbdb2f1d4e37d
freeman <the.freeman.lab@gmail.com>
2015-10-13 13:23:58 -0400

    Reorganize scripts

--- example/chrome.sh
+++ example/chrome.sh
@@ -1,9 +1,8 @@
-sudo apt-get install git -y
+sudo apt-get install git debconf-utils -y
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$PATH:/home/ubuntu/depot_tools
 echo 'export PATH=$PATH:/home/ubuntu/depot_tools' >> ~/.bashrc
-source ~/.bashrc
 fetch --nohooks --no-history chromium
-sudo apt-get install debconf-utils -y
 echo 'ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true' | sudo debconf-set-selections
 echo 'deb http://us.archive.ubuntu.com/ubuntu/ trusty multiverse' | sudo tee -a /etc/apt/sources.list 
 echo 'deb-src http://us.archive.ubuntu.com/ubuntu/ trusty multiverse' | sudo tee -a /etc/apt/sources.list
@@ -13,4 +12,4 @@ cd src
 sudo ./build/install-build-deps.sh --no-prompt
 gclient runhooks
 sudo pkill -f icecc
-sudo iceccd -u ubuntu -vv -s $ICECC_IP -n $ICECC_NAME -d
+sudo iceccd -u ubuntu -vvv -s $ICECC_IP -n $ICECC_NAME -d
--- exec/master.sh
+++ exec/master.sh
@@ -1,5 +1,5 @@
-sudo iceccd -u ubuntu -n {NAME} -s {IP} -vv -d
 echo 'export ICECC_IP={IP}' | sudo tee -a ~/.bashrc
 echo 'export ICECC_NAME={NAME}' | sudo tee -a ~/.bashrc
 echo 'export PATH="/usr/lib/icecc/bin:$PATH"' | sudo tee -a ~/.bashrc
 echo 'export GYP_DEFINES="$GYP_DEFINES clang=0 linux_use_debug_fission=0 linux_use_bundled_binutils=0"' | sudo tee -a ~/.bashrc
+sudo iceccd -u ubuntu -n {NAME} -s {IP} -vv -d
